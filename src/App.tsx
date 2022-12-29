a9a1cfa91bb7ec0c863dd0bab4c80f43f46cf7a9
Vitor Hugo <job_vitoraraujo@outlook.com>
2022-12-29 12:48:56 -0300

    Definindo variáveis no SCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
-import React from 'react';
-import './App.css';
+import './sass/App.scss';
+import { Logo } from './components/Logo';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Logo />
+      <h1>Hello World</h1>
+      <p>Teste</p>
     </div>
   );
 }
---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,7 @@
+import "./styles.scss"
+
+export const Logo = () => {
+    return (
+        <h1 className="title">Aviato</h1>
+    )
+}
